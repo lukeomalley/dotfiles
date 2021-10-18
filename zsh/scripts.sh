dc25817631a4bbd63fbb34f32c4e84b84b5e9735
Luke O'Malley <luke@lukeomalley.xyz>
2021-10-18 20:46:47 +0000

    change bindings for dmenu

--- zsh/scripts.sh
+++ zsh/scripts.sh
@@ -25,13 +25,14 @@ dualmon() {
       --output DP-1 --mode 1920x1080 --scale 2x2
 }
 
+
+singlemon() {
+  xrandr --output eDP-1 --mode 1920x1080 
+}
+
 trimon() {
   xrandr \
       --output eDP-1 --auto  \
       --output DP-1 --auto --right-of DP-2 \
       --output DP-2 --auto --right-of eDP-1 
-}
-
-singlemon() {
-  xrandr --output eDP-1 --mode 1920x1080 
 }
