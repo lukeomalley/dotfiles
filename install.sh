18bcea719b0b39dd3bfca9d658f5b2a17f10f3da
Luke OMalley <luke@lukeomalley.xyz>
2025-05-26 13:01:58 -0600

    chore: add signing key

--- install.sh
+++ install.sh
@@ -25,6 +25,7 @@ cp -rf "$DOTFILES/fonts" "$XDG_DATA_HOME"
 # git config
 mkdir -p "$XDG_CONFIG_HOME/git"
 ln -sf "$DOTFILES/git/.gitconfig" "$XDG_CONFIG_HOME"
+ln -sf "$DOTFILES/git/.gitignore" "$XDG_CONFIG_HOME"
 
 # Ranger
 mkdir -p "$XDG_CONFIG_HOME/ranger"
