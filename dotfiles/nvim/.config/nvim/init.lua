99a62a0674bd12ef3fe16ee47b90cf6c19c27e90
Luke OMalley <luke@lukeomalley.xyz>
2026-04-23 16:57:30 -0600

    feat(nvim): show dotfiles in telescope find_files and live grep
    - Enable hidden = true by default in telescope and ignore .git/
    - Pass --hidden to ripgrep via vimgrep_arguments so live grep sees dotfiles
    - Switch <leader>sf from git_files to find_files with hidden = true so
      untracked dotfiles and non-git directories are searchable

--- dotfiles/nvim/.config/nvim/init.lua
+++ dotfiles/nvim/.config/nvim/init.lua
@@ -268,11 +268,24 @@ require('gitsigns').setup {
 -- See `:help telescope` and `:help telescope.setup()`
 require('telescope').setup {
   defaults = {
+    hidden = true,
+    file_ignore_patterns = { "%.git/" },
     mappings = {
       i = {
         ['<C-u>'] = false,
         ['<C-d>'] = false,
       },
+    },
+    vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+      "--hidden",
+      "--glob", "!.git/",
     },
   },
 }
@@ -322,7 +335,7 @@ vim.keymap.set('n', '<leader>/', function()
   })
 end, { desc = '[/] Fuzzily search in current buffer]' })
 
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').git_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sf', function() require('telescope.builtin').find_files({ hidden = true }) end, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
